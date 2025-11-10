d35a0f8f17f392eb5a7627b8cd93571442990a0e
Bakhytbek <baxa.aseke@gmail.com>
2025-11-10 23:10:09 +0600

    first-lesson

---
+++ mmmm.py
@@ -0,0 +1,28 @@
+# print('heello worid')
+# num1 = int(input("введите первое число"))
+# num2 = int(input("введите второе число"))
+
+
+# print("сумма:", num1 + num2)
+# print("разность:", num1 - num2)
+# print("произведенине:",num1 * num2)
+# print("деление:" , num1 / num2)
+
+
+# # решение:
+# number = int(input ("введите число: "))
+# divisor = int(input("введите делитель: "))
+
+
+# remainder = number % divisor
+# print("остаток от деления:", remainder)
+
+
+# решение
+
+
+# решение:
+num1 = int(input("введите первое число:"))
+num2 = int(input("введите второе число: ")) 
+
+print(num1>num2)
